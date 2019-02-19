153671ed6357a8d588690bc0de1f4094c1a9c458
ortiza0556 <47364919+ortiza0556@users.noreply.github.com>
2019-02-19 00:35:48 -0500

    Added Test Class

--- Sprint1/src/GetYourShitTogether/src/GetYourShitTogether.java
+++ Sprint1/src/GetYourShitTogether/src/GetYourShitTogether.java
@@ -2,13 +2,9 @@
 public class GetYourShitTogether {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		DatabaseInterface dbi = new DatabaseInterface("C:\\Users\\Austin\\Documents\\GitHub\\COP4331-Group-16-mster\\Sprint1\\src\\GetYourShitTogether.sqlite");
 		
-		dbi.InsertIntoAnimeTable("Anohana", 2011, "Drama,Slice-of-Life", "8.53", "Boy meets ghost girl", "A-1 pictures");
-		dbi.SearchAnimeTable("Anohana");
-		dbi.close();
+		TestClass test = new TestClass();
+		test.AnimeTest();
 	}
 
 }
