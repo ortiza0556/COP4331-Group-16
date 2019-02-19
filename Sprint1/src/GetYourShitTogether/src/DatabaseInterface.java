ade4a8857d83e58243dd7a87397aa93619082a8f
asedwards <45155057+asedwards@users.noreply.github.com>
2019-02-18 22:37:10 -0500

    made connect and close in databse interface

--- Sprint1/src/GetYourShitTogether/src/DatabaseInterface.java
+++ Sprint1/src/GetYourShitTogether/src/DatabaseInterface.java
@@ -1,4 +1,43 @@
 
-public class DatabaseInterface {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+
+public class DatabaseInterface {
+	
+		private static String filePath;
+		private static Connection conn;
+	
+		public DatabaseInterface (String fp) {
+			
+			filePath = fp;
+			connect();
+			
+		}
+		
+		private static void connect() {
+			
+			try {
+				
+				String database = "jdbc:sqlite:" + filePath;
+				conn = DriverManager.getConnection(database);
+				
+				System.out.println("Connection made");
+				
+			} catch (SQLException e) {
+				System.out.println("I AM AN ERROR MESSAGE" +e.getMessage());
+			}
+		}
+		
+		public void close() {
+			try {
+				conn.close();
+				
+				System.out.println("Connection closed");
+				
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
 }
