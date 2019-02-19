a1dfb3d8dbb54eaabd19cd1e4a61a36d0285bb8a
asedwards <45155057+asedwards@users.noreply.github.com>
2019-02-18 22:37:20 -0500

    added test code for databse interface

--- Sprint1/src/GetYourShitTogether/src/GetYourShitTogether.java
+++ Sprint1/src/GetYourShitTogether/src/GetYourShitTogether.java
@@ -4,6 +4,8 @@ public class GetYourShitTogether {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		DatabaseInterface dbi = new DatabaseInterface("C:\\Users\\Austin\\Documents\\GitHub\\COP4331-Group-16-mster\\Sprint1\\src\\GetYourShitTogether.sqlite");
+		dbi.close();
 	}
 
 }
