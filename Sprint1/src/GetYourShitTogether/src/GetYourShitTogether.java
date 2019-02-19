de49f36e15250461fa6bdec664848fc7580bdb63
asedwards <45155057+asedwards@users.noreply.github.com>
2019-02-19 00:01:30 -0500

    added tests ffor insert and fetch

--- Sprint1/src/GetYourShitTogether/src/GetYourShitTogether.java
+++ Sprint1/src/GetYourShitTogether/src/GetYourShitTogether.java
@@ -5,6 +5,9 @@ public class GetYourShitTogether {
 		// TODO Auto-generated method stub
 
 		DatabaseInterface dbi = new DatabaseInterface("C:\\Users\\Austin\\Documents\\GitHub\\COP4331-Group-16-mster\\Sprint1\\src\\GetYourShitTogether.sqlite");
+		
+		dbi.InsertIntoAnimeTable("Anohana", 2011, "Drama,Slice-of-Life", "8.53", "Boy meets ghost girl", "A-1 pictures");
+		dbi.SearchAnimeTable("Anohana");
 		dbi.close();
 	}
 
