c9a01cf795defa1ee80e085d184c368fcca92f2c
ortiza0556 <47364919+ortiza0556@users.noreply.github.com>
2019-03-12 21:45:43 -0400

    TestClass addition
    Slight changes to test class

--- Sprint2/src/GetYourShitTogether/src/TestClass.java
+++ Sprint2/src/GetYourShitTogether/src/TestClass.java
@@ -1,6 +1,9 @@
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class TestClass {
 
+	@Test
 	public void AnimeTest() {
 		// TODO Auto-generated method stub
 
