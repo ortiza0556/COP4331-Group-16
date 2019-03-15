4ae4f656a38fb2c06319f4a03f8471d65d6390b8
Andrew Pepin <andrewkpepin@gmail.com>
2019-03-15 16:13:25 -0400

    added backlog class (per system arch design)

---
+++ Sprint2/src/GetYourShitTogether/src/Backlog.java
@@ -0,0 +1,27 @@
+import 
+
+public class Backlog extends Media {
+
+	// NEED TO INITIALIZE CONTAINER OF BACKLOG ITEMS (using List? Array? Arraylist?)
+	private int size;
+	
+	public Backlog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void editEntry(Media BacklogItem) {
+		
+	}
+	
+	public void store(Media BacklogItem) {
+		
+	}
+	
+	public BacklogItem retrieve() {
+		return null;
+	}
+	
+	public void display() {
+		
+	}
+}
