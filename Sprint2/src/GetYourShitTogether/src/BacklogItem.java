11e456d0f16cda3d0d87f7ad0442ef9566c3e1ff
Andrew Pepin <andrewkpepin@gmail.com>
2019-03-15 15:43:52 -0400

    added backlogItem class (per system arch design)

---
+++ Sprint2/src/GetYourShitTogether/src/BacklogItem.java
@@ -0,0 +1,17 @@
+
+public class BacklogItem extends Media {
+
+	private int databaseID;
+	
+	public BacklogItem(Media mediaItem) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getDatabaseID() {
+		return databaseID;
+	}
+
+	public void setDatabaseID(int databaseID) {
+		this.databaseID = databaseID;
+	}
+}
