7e7de8305d02251e9631c8e1b1c94de20a53fd1f
ortiza0556 <47364919+ortiza0556@users.noreply.github.com>
2019-03-17 00:21:58 -0400

    Updated MovieBacklog

--- Sprint2/src/GetYourShitTogether/src/MovieBacklog.java
+++ Sprint2/src/GetYourShitTogether/src/MovieBacklog.java
@@ -3,7 +3,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MovieBacklog <M extends Media>{
 	
