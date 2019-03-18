7d317a45a581c7533e795485154f2556ed0d3b16
asedwards <45155057+asedwards@users.noreply.github.com>
2019-03-17 23:11:21 -0400

    made many changes. so many. kill me

---
+++ Sprint2/src/GetYourShitTogether/src/AnimeBacklogItem.java
@@ -0,0 +1,45 @@
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class AnimeBacklogItem {
+	
+	private SimpleStringProperty title;
+    private SimpleStringProperty genre;
+    private SimpleStringProperty status;
+    private SimpleStringProperty rating;
+    private SimpleIntegerProperty priority;
+ 
+    public AnimeBacklogItem (String title, String genre, String status, String rating, int priority) {
+ 
+        this.title = new SimpleStringProperty(title);
+        this.genre = new SimpleStringProperty(genre);
+        this.status = new SimpleStringProperty(status);
+        this.rating = new SimpleStringProperty(rating);
+        this.priority = new SimpleIntegerProperty(priority);
+    }
+ 
+    public String getTitle() {
+ 
+        return title.get();
+    }
+    
+    public String getGenre() {
+ 
+        return genre.get();
+    }
+    
+    public String getStatus() {
+    	
+    	return status.get();
+    }
+    
+    public String getRating() {
+    	
+    	return rating.get();
+    }
+    
+    public int getPriority() {
+    	
+    	return priority.get();
+    }
+}
--- Sprint2/src/GetYourShitTogether/src/TVShowBacklog.java
+++ Sprint2/src/GetYourShitTogether/src/TVShowBacklog.java
@@ -3,6 +3,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class TVShowBacklog {
 
@@ -148,4 +152,37 @@ public class TVShowBacklog {
         return 1;
         
     }
+	
+	public ObservableList<TVShowBacklogItem> fetchAll() {
+		
+		 String sql = "SELECT Title,Genre,Status,UserRating,Priority FROM TVShows JOIN TVShows_backlog ON TVShows.TVID IS TVShows_backlog.TVID";
+	        
+	        ObservableList<TVShowBacklogItem> result = FXCollections.observableArrayList();
+	        boolean added = false;
+	        
+	        try {
+	            Statement stmt = this.conn.createStatement();
+	            
+	            ResultSet rs = stmt.executeQuery(sql);
+	            
+	            while(rs.next()) {
+	            	TVShowBacklogItem currTVShow = new TVShowBacklogItem(rs.getString("Title"),rs.getString("Genre"),rs.getString("Status"),rs.getString("UserRating"),rs.getInt("Priority"));
+	            	
+	            	added = result.add(currTVShow);
+	            	
+	            	if(!added){
+	            		System.out.println("Error, tvshow not added properly");
+	            	}else {
+	            		added = false;
+	            	}
+	            }
+	            
+	            return result;
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+	        
+	        return null;
+		
+	}
 }
