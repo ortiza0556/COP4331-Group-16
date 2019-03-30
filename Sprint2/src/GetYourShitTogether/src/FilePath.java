d4590e156d00d4f39358b36e2bee7dc86287c29b
ortiza0556 <47364919+ortiza0556@users.noreply.github.com>
2019-03-30 18:49:08 -0400

    Updated filepath to be relative, so we dont have to keep changing

--- Sprint2/src/GetYourShitTogether/src/FilePath.java
+++ Sprint2/src/GetYourShitTogether/src/FilePath.java
@@ -1,7 +1,7 @@
 
 public class FilePath {
 	
-	protected String filePath = "C:\\Users\\aubre\\Documents\\GitHub\\COP4331-Group-16\\Sprint3\\src\\getYourShitTogether.sqlite";
+	protected String filePath = "..\\getYourShitTogether.sqlite";
 	
 	public FilePath() {
 		
