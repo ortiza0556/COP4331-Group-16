cf7b37116b10f4f1934e731a041842368387ba40
Andrew Pepin <andrewkpepin@gmail.com>
2019-04-07 19:07:22 -0400

    Fixed JUnit test cases (they work now :>)

--- Sprint3/src/GetYourShitTogether/src/FilePath.java
+++ Sprint3/src/GetYourShitTogether/src/FilePath.java
@@ -1,7 +1,7 @@
 
 public class FilePath {
 	
-	protected String filePath = "..\\getYourShitTogether.sqlite";
+	protected String filePath = "C:\\Users\\Pepin\\Desktop\\repo\\COP4331-Group-16\\Sprint3\\src\\getYourShitTogether.sqlite";
 	
 	public FilePath() {
 		
